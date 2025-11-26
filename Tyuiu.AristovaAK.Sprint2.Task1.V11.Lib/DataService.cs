d51d07218f7fecf53415b525042358384f537fb9
AristovaAlina <alinaaristova72@gmail.com>
2025-11-27 03:19:52 +0500

    Task1

---
+++ Tyuiu.AristovaAK.Sprint2.Task1.V11.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.AristovaAK.Sprint2.Task1.V11.Lib
+{
+    public class DataService : ISprint2Task1V11
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool[] res = new bool[6];
+
+            /// ==, !=, <, >, <=, >=
+            /// |,       &,     ||,   &&,   !,    ^
+            /// False, False, True, False, True, False
+            /// a = 145, b = 156, c = 142, d = 117
+
+            res[0] = (a > b) | (c == d);
+            res[1] = (a != c) & (b == d);
+            res[2] = (a <= b) || (d == c);
+            res[3] = (a == b) && (c < d);
+            res[4] = !(a == d);
+            res[5] = (a < b) ^ (c > d);
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.AristovaAK.Sprint2.Task1.V11/Program.cs
@@ -0,0 +1,50 @@
+﻿using Tyuiu.AristovaAK.Sprint2.Task1.V11.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #2 | Выполнила: Аристова А. К. | СМАРТб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Логические операции                                               *");
+        Console.WriteLine("* Задание #1                                                              *");
+        Console.WriteLine("* Вариант #11                                                             *");
+        Console.WriteLine("* Выполнила: Аристова Алина Константиновна | СМАРТб-25-1                  *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,         *");
+        Console.WriteLine("* последовательность можно чередовать, но использовать один раз в         *");
+        Console.WriteLine("* выражении) и логических операций (|, &, ||, &&, !, ^,                   *");
+        Console.WriteLine("* последовательность операций не должна нарушаться), а также              *");
+        Console.WriteLine("* арифметических выражений, которая вернет логическую последовательность  *");
+        Console.WriteLine("* (массив): (False, False, True, False, True, False), при a = 145,        *");
+        Console.WriteLine("* b = 156, c = 142, d = 117                                               *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int a = 145;
+        int b = 156; 
+        int c = 142;
+        int d = 117;
+        Console.WriteLine("a = " + a);
+        Console.WriteLine("b = " + b);
+        Console.WriteLine("c = " + c);
+        Console.WriteLine("d = " + d);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        bool[] res = ds.GetLogicOperations(a, b, c, d);
+
+        for (int i = 0; i < 6; i++)
+        {
+            Console.WriteLine(res[i]);
+        }
+
+        Console.ReadKey();
+    }
+}
